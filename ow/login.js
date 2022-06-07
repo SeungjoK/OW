aceff2fec8167ba4465a1a6274fac836cca5a9b7
SeungjoK <106789077+SeungjoK@users.noreply.github.com>
2022-06-07 20:33:59 +0900

    Add files via upload

---
+++ ow/login.js
@@ -0,0 +1,19 @@
+document.querySelector("#btn").addEventListener("click", () => {
+    const id = "ow";
+    const password = "1234";
+ 
+    if(id == document.querySelector("#username").value) {
+        if(password == document.querySelector("#password").value) {
+            alert("환영합니다!");
+            //document.write("환영합니다!");
+        }
+        else {
+            alert("비밀번호가 맞지 않습니다.");
+            //document.write("비밀번호가 맞지 않습니다.");
+        }
+    }
+    else {
+        alert("아이디 혹은 비밀번호가 맞지 않습니다.");
+        //document.wrtie("아이디 혹은 비밀번호가 맞지 않습니다.");
+    }
+});
